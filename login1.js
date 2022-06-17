3c042eb6f8f1a4abdd2cf13fbbc3d4513bfe13a7
meetsouhardya <meetsouhardya@gmail.com>
2022-06-17 20:23:26 +0530

    Step 1 to integrate a login page to the ArtOfOwn website

---
+++ login1.js
@@ -0,0 +1,10 @@
+function check(){
+    let a=document.getElementById('username').value;
+    let b=document.getElementById('password').value;
+    if(a=="souhardya" && b=="sd123"){
+        alert("Login Successfull")
+    }
+    else{
+        alert("Login Fail");
+    }
+}
